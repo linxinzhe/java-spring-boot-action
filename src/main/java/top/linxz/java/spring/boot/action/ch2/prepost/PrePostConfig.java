65650cc6411c26af87fbdcea040bd7a86407934c
linxinzhe <linxinzhechn@qq.com>
2018-04-23 16:05:32 +0800

    feat(ch2): init destroy bean method

---
+++ src/main/java/top/linxz/java/spring/boot/action/ch2/prepost/PrePostConfig.java
@@ -0,0 +1,20 @@
+package top.linxz.java.spring.boot.action.ch2.prepost;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("top.linxz.java.spring.boot.action.ch2.prepost")
+public class PrePostConfig {
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    BeanWayService beanWayService() {
+        return new BeanWayService();
+    }
+
+    @Bean
+    JSR250WayService jsr250WayService() {
+        return new JSR250WayService();
+    }
+}
