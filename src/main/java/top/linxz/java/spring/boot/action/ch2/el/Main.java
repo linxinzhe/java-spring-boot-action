9601596243c556a467f72c3ffbd3faf20e48ac26
linxinzhe <linxinzhechn@qq.com>
2018-04-23 15:55:05 +0800

    feat(ch2): el config

---
+++ src/main/java/top/linxz/java/spring/boot/action/ch2/el/Main.java
@@ -0,0 +1,12 @@
+package top.linxz.java.spring.boot.action.ch2.el;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ElConfig.class);
+        ElConfig elConfig = context.getBean(ElConfig.class);
+        elConfig.outputResource();
+        context.close();
+    }
+}
