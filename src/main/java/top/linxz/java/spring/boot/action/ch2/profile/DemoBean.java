f4fe7767ce39a780bb40a98c3146ae67aa8dde96
linxinzhe <linxinzhechn@qq.com>
2018-04-23 17:03:52 +0800

    feat(ch2): dev or production env

---
+++ src/main/java/top/linxz/java/spring/boot/action/ch2/profile/DemoBean.java
@@ -0,0 +1,17 @@
+package top.linxz.java.spring.boot.action.ch2.profile;
+
+public class DemoBean {
+    private String content;
+
+    public DemoBean(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
