185544dc041d09668983f2b8a53f29b5cb6da6a7
linxinzhe <linxinzhechn@qq.com>
2018-04-24 09:38:15 +0800

    feat(ch3): combination  annotation

---
+++ src/main/java/top/linxz/java/spring/boot/action/ch3/annotation/DemoService.java
@@ -0,0 +1,10 @@
+package top.linxz.java.spring.boot.action.ch3.annotation;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+    public void outputResult() {
+        System.out.println("从组合注解配置照样获得bean");
+    }
+}
